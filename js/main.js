e2b18df841d348ff9b61e7e90e2f2d801ad79e8a
Konrad Dzwinel <kdzwinel@gmail.com>
2015-01-07 14:59:29 +0100

    Default questions updated.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
     window.appState = new State();
     var defaultState = {
         year: 2014,
-        question1: 1,
-        question2: 2,
+        question1: 3,
+        question2: 7,
         minLinkValue: 10,
         splitMultipleAnswers: true
     };
